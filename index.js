1058263a70e622b3da1855f461a8f0b6101b084d
Akin Mert Bur <akinmertbur@gmail.com>
2024-04-29 23:39:54 +0300

    Enhanced functionality: integrated Geocoding API to fetch latitude and longitude for user-inputted locations. Implemented OpenUV API integration to dynamically retrieve UV index data, enabling real-time determination of sunscreen necessity. Also, seamlessly passed the acquired UV index data to index.ejs for enhanced user experience.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-import express from "express";
+import express, { response } from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
@@ -9,12 +9,68 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+// Determine API_URLs and API_KEYs.
+const GEOCODING_API_URL = "https://www.mapquestapi.com/geocoding/v1/address";
+const OPENUV_API_URL = "https://api.openuv.io/api/v1/uv";
+const apiKeyGeocoding = process.env.GEOCODING_API_KEY;
+const apiKeyOpenuv = process.env.OPENUV_API_KEY;
+
 // Use the public folder for static files.
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Use Axios to retrieve the geographical coordinates of the location.
+const getCoordinates = async function (location) {
+  try {
+    const response = await axios.get(GEOCODING_API_URL, {
+      params: { key: apiKeyGeocoding, location: location },
+    });
+    const latitude = response.data.results[0].locations[0].latLng.lat;
+    const longitude = response.data.results[0].locations[0].latLng.lng;
+    const arrCoordinates = [latitude, longitude];
+    return arrCoordinates;
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    return;
+  }
+};
+
+// When the user goes to the home page it should render the index.ejs file.
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+// Add a feature to the homepage (index.ejs) that determines
+// whether sunscreen is needed today based on the UV index data.
+app.post("/", async (req, res) => {
+  const location = req.body["location"];
+
+  // Retrieve the geographical coordinates of the location.
+  const coordinates = await getCoordinates(location);
+
+  // Use Axios to retrieve the UV index data and pass it to
+  // index.ejs to display whether sunscreen is needed today.
+  try {
+    const response = await axios.get(OPENUV_API_URL, {
+      headers: {
+        "x-access-token": apiKeyOpenuv,
+      },
+
+      params: {
+        lat: coordinates[0],
+        lng: coordinates[1],
+      },
+    });
+    console.log(response.data.result.uv);
+    res.render("index.ejs", {
+      content: response.data.result.uv,
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 // Listen on the predefined port and start the server.
